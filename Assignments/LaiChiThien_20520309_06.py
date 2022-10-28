588921394757aec4e304f9b6e7c389eef8f7aa5d
ChiThien <74474150+laichithien@users.noreply.github.com>
2022-10-28 17:43:28 +0700

    update

--- Assignments/LaiChiThien_20520309_06.py
+++ Assignments/LaiChiThien_20520309_06.py
@@ -1,3 +1,5 @@
+
+from unittest import result
 import streamlit as st
 import pandas as pd 
 import numpy as np
@@ -11,11 +13,13 @@ from sklearn.model_selection import KFold
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
 st.title("HỆ THỐNG PHÂN TÍCH DỮ LIỆU TỰ ĐỘNG")
+import math
 
-uploaded_file = st.file_uploader("Chọn ảnh đi bro")
+uploaded_file = st.file_uploader("Upload dữ liệu của bạn tại đây")
 if not uploaded_file: 
-    st.warning("Please input a dataset")
+    st.warning("Up đi ạ")
     st.stop()
 # Nếu tồn tại file
 if uploaded_file is not None:
@@ -54,53 +58,95 @@ output_list = [header for header in list_header if header not in feature_list_ro
 output = st.selectbox('Choose your output: ', output_list) # Output được lưu trong output 
 
 st.header("Data Preprocessing")
-train_test_split_col, k_folder_col = st.columns(2)
-
-dataset_split = st.radio('Chọn cách chia dữ liệu huấn luyện', ['Train/test split', 'K Fold'])
-if not dataset_split: 
-    st.stop()
-
-if dataset_split == 'Train/test split':
-    train_size = st.slider('Train', 0, 100, format='%d%%') # Train size được lưu trong train_size
-elif dataset_split == 'K Fold':
-    k = st.slider('Folds', 0, 10) # K folder (nếu có được lưu trong k)
-
-
 
 ohe = OneHotEncoder()
 transformed = ohe.fit_transform(data_train_ob)
 transformed = pd.DataFrame(transformed.astype(int).toarray())
-st.table(transformed.iloc[0:10])
-
 data_train = pd.concat([data_train_num, transformed], axis=1)
-
 data_test =  dataset[output]
-st.table(data_train.iloc[0: 10])
-# Splitting strategy
-if dataset_split == 'Train/test split':
+
+
+train_test_split_col, k_folder_col = st.columns(2)
+with train_test_split_col:
+    st.header("Train/test split")
+    st.header("     ")
+    train_size = st.slider('Train', 0, 100, format='%d%%') # Train size được lưu trong train_size
+    
+with k_folder_col:
+    st.header("K Fold Cross-validation")
+    k = st.slider('Folds', 0, 10)
+    
+result1, result2 = st.columns(2)
+with result1:
     if not train_size:
         st.stop()
     X_train, X_test, y_train, y_test = train_test_split(data_train, data_test, train_size=train_size/100)
-
-elif dataset_split == 'K Fold':
+    reg = LinearRegression()
+    if y_train.dtype == object:
+        st.stop()
+    reg.fit(X_train, y_train)
+    # st.write(y_test)
+    y_pred = reg.predict(X_test)
+    # st.write(y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    st.write("MAE:", mae)
+    mse = mean_squared_error(y_test, y_pred)
+    st.write("MSE:", mse)
+    r2 = r2_score(y_test, y_pred)
+    st.write("R2:", r2)
+with result2:
+    if not k:
+        st.stop()
     kf = KFold(n_splits=k)
     kf.get_n_splits(data_train)
+    model_list = []
+    mae_list = []
+    mse_list = []
+    r2_list = []
+    for train_index, test_index in kf.split(data_train):
+        X_train, X_test = data_train.values[train_index], data_train.values[test_index]
+        y_train, y_test = data_test.values[train_index], data_test.values[test_index]
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        mae = mean_absolute_error(y_test, y_pred)
+        mse = mean_squared_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+        model_list.append(model)
+        mae_list.append(mae)
+        mse_list.append(mse)
+        r2_list.append(r2)
+    mae_list = pd.DataFrame(mae_list)
+    mse_list = pd.DataFrame(mse_list)
+    r2_list = pd.DataFrame(r2_list)
+    avg_mae = sum(mae_list[0])/len(mae_list)
+    avg_mse = sum(mse_list[0])/len(mse_list)
+    avg_r2 = sum(r2_list[0])/len(r2_list)
+    st.write('Average mae:', avg_mae)
+    st.write('Average mse:', avg_mse)
+    st.write('Average r2:', avg_r2)
+# if dataset_split == 'Train/test split':
+#     train_size = st.slider('Train', 0, 100, format='%d%%') # Train size được lưu trong train_size
+# elif dataset_split == 'K Fold':
+     # K folder (nếu có được lưu trong k)
 
-reg = LinearRegression()
 
-reg.fit(X_train, y_train)
 
-# st.write(y_test)
-y_pred = reg.predict(X_test)
-# st.write(y_pred)
-mae = mean_absolute_error(y_test, y_pred)
-st.write("MAE:", mae)
-mse = mean_squared_error(y_test, y_pred)
-st.write("MSE:", mse)
-table1, table2 = st.columns(2)
 
-with table1: 
-    st.table(y_test)
-with table2:
-    st.table(y_pred)
+# st.table(data_train.iloc[0: 10])
+# Splitting strategy
+# if dataset_split == 'Train/test split':
+#     if not train_size:
+#         st.stop()
+#     X_train, X_test, y_train, y_test = train_test_split(data_train, data_test, train_size=train_size/100)
+
+
+# elif dataset_split == 'K Fold':
+    
+
+# st.table(y_train)
+
+
+
+
 
