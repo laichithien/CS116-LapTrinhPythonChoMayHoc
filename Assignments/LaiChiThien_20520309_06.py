6f5c37529908564142398349314d893bf19a611f
ChiThien <74474150+laichithien@users.noreply.github.com>
2022-10-28 15:53:34 +0700

    update

--- Assignments/LaiChiThien_20520309_06.py
+++ Assignments/LaiChiThien_20520309_06.py
@@ -5,7 +5,12 @@ from PIL import Image
 import os
 import seaborn as sb
 from sklearn.linear_model import LinearRegression
-
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
 st.title("HỆ THỐNG PHÂN TÍCH DỮ LIỆU TỰ ĐỘNG")
 
 uploaded_file = st.file_uploader("Chọn ảnh đi bro")
@@ -28,15 +33,24 @@ feature_used = []
 for i in range(len(list_header)):
     with checkboxes_feature[i]:
         feature_used.append(st.checkbox(list_header[i]))
-st.header('Features selected: ')
-feature_list = []
+# st.header('Features selected: ')
+feature_list_root = []
 for i in range(len(list_header)):
-    if feature_used[i]: # Features được dùng lưu trong feature_list
-        st.write(list_header[i]) 
-        feature_list.append(list_header[i])
+    if feature_used[i]: # Features được dùng lưu trong feature_list_root
+        # st.write(list_header[i]) 
+        feature_list_root.append(list_header[i])
+
+if not feature_list_root:
+    st.stop()
+feature_list = [feature for feature in feature_list_root if not dataset[feature].dtype == object]
+nan_list = [header for header in dataset.columns if dataset[header].dtype == object and header in feature_list_root]
+data_train_num = dataset.loc[:, feature_list]
+data_train_ob = dataset.loc[:, nan_list]
+st.table(pd.concat([data_train_num, data_train_ob], axis=1).iloc[0:10])
+# st.table(data_train_ob.iloc[0:10])
 # Lấy output
 st.header("Output: ")
-output_list = [header for header in list_header if header not in feature_list]
+output_list = [header for header in list_header if header not in feature_list_root]
 output = st.selectbox('Choose your output: ', output_list) # Output được lưu trong output 
 
 st.header("Data Preprocessing")
@@ -51,4 +65,42 @@ if dataset_split == 'Train/test split':
 elif dataset_split == 'K Fold':
     k = st.slider('Folds', 0, 10) # K folder (nếu có được lưu trong k)
 
+
+
+ohe = OneHotEncoder()
+transformed = ohe.fit_transform(data_train_ob)
+transformed = pd.DataFrame(transformed.astype(int).toarray())
+st.table(transformed.iloc[0:10])
+
+data_train = pd.concat([data_train_num, transformed], axis=1)
+
+data_test =  dataset[output]
+st.table(data_train.iloc[0: 10])
+# Splitting strategy
+if dataset_split == 'Train/test split':
+    if not train_size:
+        st.stop()
+    X_train, X_test, y_train, y_test = train_test_split(data_train, data_test, train_size=train_size/100)
+
+elif dataset_split == 'K Fold':
+    kf = KFold(n_splits=k)
+    kf.get_n_splits(data_train)
+
+reg = LinearRegression()
+
+reg.fit(X_train, y_train)
+
+# st.write(y_test)
+y_pred = reg.predict(X_test)
+# st.write(y_pred)
+mae = mean_absolute_error(y_test, y_pred)
+st.write("MAE:", mae)
+mse = mean_squared_error(y_test, y_pred)
+st.write("MSE:", mse)
+table1, table2 = st.columns(2)
+
+with table1: 
+    st.table(y_test)
+with table2:
+    st.table(y_pred)
 
