75b45d7435f790313b4e2e62d3508dc07b51fc16
ChiThien <74474150+laichithien@users.noreply.github.com>
2022-10-28 21:13:56 +0700

    k

--- Assignments/LaiChiThien_20520309_06.py
+++ Assignments/LaiChiThien_20520309_06.py
@@ -5,7 +5,6 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 import os
-import seaborn as sb
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
